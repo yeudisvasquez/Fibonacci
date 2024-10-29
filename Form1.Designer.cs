e4f1559bdbaec857e9219a61bf74e3cfc9bf737c
yeudisvasquez <yhvherrera@gmail.com>
2024-10-29 19:40:52 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Fibonacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtvalor1 = new System.Windows.Forms.TextBox();
+            this.txtvalor2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(144, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Empezar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(136, 150);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(263, 144);
+            this.listBox1.TabIndex = 1;
+            // 
+            // txtvalor1
+            // 
+            this.txtvalor1.Location = new System.Drawing.Point(241, 69);
+            this.txtvalor1.Name = "txtvalor1";
+            this.txtvalor1.Size = new System.Drawing.Size(100, 26);
+            this.txtvalor1.TabIndex = 1;
+            // 
+            // txtvalor2
+            // 
+            this.txtvalor2.Location = new System.Drawing.Point(241, 104);
+            this.txtvalor2.Name = "txtvalor2";
+            this.txtvalor2.Size = new System.Drawing.Size(100, 26);
+            this.txtvalor2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(298, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Valor Inicial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(140, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Valor Final";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(534, 376);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtvalor2);
+            this.Controls.Add(this.txtvalor1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Secuencia Fibonacci";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtvalor1;
+        private System.Windows.Forms.TextBox txtvalor2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtvalor1.Text, out int start) && int.TryParse(txtvalor2.Text, out int end))
+            {
+                // Validate start and end values
+                if (start <= 0 || end <= 0 || end < start)
+                {
+                    MessageBox.Show("Por favor introduzca un valor en el segundo text que sea mayor al valor que el del primer text.");
+                    txtvalor1.Clear();
+                    txtvalor2.Clear();
+                    return;
+                }
+
+                // Calculate Fibonacci sequence
+                int a = start, b = start, c;
+
+                listBox1.Items.Add(a); // Start the sequence with 0
+
+                while (b <= end)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    listBox1.Items.Add(a);
+                    /*
+                    if (a >= start && a <= end)
+                    {
+                        listBox1.Items.Add(a);
+                    }*/
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor introduzca valores enteros.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtvalor1.Clear();
+            txtvalor2.Clear();
+            listBox1.Items.Clear();
+        }
+    }
+}
+
+/*
+ * Explanation 
+-The btnGenerate_Click method retrieves the start and end values from txtStart and txtEnd textboxes.
+-It validates the input values to ensure they are valid integers and the end value is greater than or equal to the start value.
+-It then calculates Fibonacci numbers starting from 0 until the Fibonacci number is greater than the end value or less than the start value.
+-Valid Fibonacci numbers within the specified range are added to the fibonacciSequence list.
+-Finally, it binds this list to a ListBox (assuming you have a ListBox named listBoxSequence) to display the Fibonacci sequence.
+*/
